20c2f3550532eb9e86c4fc2803fe9e266df6e110
Jonathan Karr <jonrkarr@gmail.com>
2016-11-03 20:33:32 -0400

    removing N_AVOGADRO which is redundant with scipy.constants.Avogadro

--- wc_utils/util/misc_utils.py
+++ wc_utils/util/misc_utils.py
@@ -10,8 +10,6 @@
 
 import numpy as np
 
-N_AVOGADRO = 6.022e23 #Avogadro constant
-
 def nanminimum(x, y):
     return np.where(np.logical_or(np.isnan(y), np.logical_and(x <= y, np.logical_not(np.isnan(x)))), x, y)
     

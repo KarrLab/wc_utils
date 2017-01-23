a427f0dba9a94a5fd7b92d9185419477dcc59735
Arthur Goldberg <artgoldberg@gmail.com>
2017-01-23 15:59:45 -0500

    commit with debugging output

--- wc_utils/schema/io.py
+++ wc_utils/schema/io.py
@@ -342,6 +342,9 @@ class Reader(object):
             data = transpose(data)
         headings = headings[0]
 
+        print('model', model)
+        print('headings', headings)
+        print('data', data)
         # prohibit duplicate headers
         header_map = defaultdict(list)
         for heading in headings:

572f22f6c8be3cfa470e1c29938a31a5532bb60b
Jonathan Karr <jonrkarr@gmail.com>
2017-05-26 18:30:48 -0400

    adding weighted_percentile, weighted_mean; adding tests for ExponentialMovingAverage

---
+++ tests/util/test_stats.py
@@ -0,0 +1,87 @@
+""" Statistic utilities tests
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2017-05-26
+:Copyright: 2017, Karr Lab
+:License: MIT
+"""
+
+from wc_utils.util import stats
+import numpy
+import numpy.testing
+import unittest
+
+
+class TestStats(unittest.TestCase):
+
+    def test_ExponentialMovingAverage(self):
+        exp = stats.ExponentialMovingAverage(1., alpha=0.)
+        self.assertEqual(exp.value, 1.)
+        self.assertEqual(exp.alpha, 0.)
+
+        exp = stats.ExponentialMovingAverage(1., center_of_mass=0.)
+        self.assertEqual(exp.value, 1.)
+        self.assertEqual(exp.alpha, 1.)
+
+        exp = stats.ExponentialMovingAverage(1., alpha=0.)
+        self.assertEqual(exp.add_value(2), 1.)
+        self.assertEqual(exp.add_value(3), 1.)
+
+        exp = stats.ExponentialMovingAverage(1., alpha=1.)
+        self.assertEqual(exp.add_value(2), 2.)
+        self.assertEqual(exp.add_value(3), 3.)
+
+        exp = stats.ExponentialMovingAverage(1., alpha=0.5)
+        self.assertEqual(exp.add_value(2), 1.5)
+        self.assertEqual(exp.add_value(3), 2.25)
+
+    def test_weighted_percentile(self):
+        self.assertEqual(stats.weighted_percentile([2], [1], 0), 2.)
+        self.assertEqual(stats.weighted_percentile([2], [1], 50), 2.)
+        self.assertEqual(stats.weighted_percentile([2], [1], 100), 2.)
+
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 1], 0), 1.)
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 1], 25), 1.)
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 1], 50), 1.5)
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 1], 75), 2.)
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 1], 100), 2.)
+
+        self.assertEqual(stats.weighted_percentile([2, 1], [10, 1], 20), 2.)
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 10], 60), 1.)
+
+        self.assertEqual(stats.weighted_percentile([2, 1, 3], [1, 1, 1], 0), 1)
+        self.assertEqual(stats.weighted_percentile([2, 1, 3], [1, 1, 1], 20), 1)
+        self.assertEqual(stats.weighted_percentile([2, 1, 3], [1, 1, 1], 50), 2)
+        self.assertEqual(stats.weighted_percentile([2, 1, 3], [1, 1, 1], 80), 3)
+        self.assertEqual(stats.weighted_percentile([2, 1, 3], [1, 1, 1], 100), 3)
+
+        self.assertEqual(stats.weighted_percentile([2, 1], [1, 0], 50), 2)
+        numpy.testing.assert_equal(stats.weighted_percentile([2, 1], [0, 0], 100), float('nan'))
+
+    def test_weighted_median(self):
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 1, 1]), numpy.median([2., 1., 3.]))
+        self.assertEqual(stats.weighted_median([2, 1, 3, 4], [1, 1, 1, 1]), numpy.median([2., 1., 3., 4.]))
+
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 0, 0]), 2.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [0, 1, 0]), 1.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [0, 0, 1]), 3.0)
+
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 1, 0]), 1.5)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [0, 1, 1]), 2.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 0, 1]), 2.5)
+
+        self.assertEqual(stats.weighted_median([2, 1, 3], [2, 1, 1]), 2.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 1, 2]), 2.5)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 2, 1]), 1.5)
+
+        self.assertEqual(stats.weighted_median([2, 1, 3], [3, 1, 1]), 2.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 1, 3]), 3.0)
+        self.assertEqual(stats.weighted_median([2, 1, 3], [1, 3, 1]), 1.0)
+
+        self.assertEqual(stats.weighted_median([2, 1], [1, 1]), 1.5)
+        self.assertEqual(stats.weighted_median([2, 1], [2, 1]), 2.0)
+        self.assertEqual(stats.weighted_median([2, 1], [1, 2]), 1.0)
+
+        numpy.testing.assert_equal(stats.weighted_median([2, 1, 3], [0, 0, 0]), float('nan'))
+        numpy.testing.assert_equal(stats.weighted_median([2], [1]), 2)
+        numpy.testing.assert_equal(stats.weighted_median([2], [0]), float('nan'))
--- wc_utils/util/stats.py
+++ wc_utils/util/stats.py
@@ -1,16 +1,13 @@
 """ Statistical utilities.
 
 :Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
-:Date: 2016-10-05
-:Copyright: 2016, Karr Lab
+:Author: Jonathan Karr <jonrkarr@mssm.edu>
+:Date: 2017-05-26
+:Copyright: 2016-2017, Karr Lab
 :License: MIT
 """
 
-from numpy import random as numpy_random
-from random import Random
-import math
-import numpy as np
-import sys
+import numpy
 
 
 class ExponentialMovingAverage(object):
@@ -18,49 +15,116 @@ class ExponentialMovingAverage(object):
 
     Each moving average S is computed recursively from the sample values Y:
         S_1 = Y_1
-        S_t = alpha * Y_t + (1-alpha)*S_(t-1)
+        S_t = alpha * Y_t + (1 - alpha) * S_(t-1)
 
     Attributes:
-        alpha: float; the decay factor
+        value (:obj:`float`): the current average
+        alpha (:obj:`float`): the decay factor        
     """
-    def __init__( self, initial_value, alpha=None, center_of_mass=None ):
+
+    def __init__(self, value, alpha=None, center_of_mass=None):
         """Initialize an ExponentialMovingAverage.
 
         Args:
-            alpha: float; the decay factor
-            center_of_mass: number; a center of mass for initializing alpha, the decay factor
-                in an exponential moving average. alpha = 1/center_of_mass
+            value (:obj:`float`): initial average value
+            alpha (:obj:`float`): the decay factor [0, 1]
+            center_of_mass (:obj:`float`): a center of mass for initializing alpha, the decay factor
+                in an exponential moving average. :obj:`alpha` =  1 / (1 + :obj:`center_of_mass`)
 
         Raises:
-            ValueError if alpha <= 0 or 1 <= alpha
+            :obj:`ValueError`: if alpha < 0 or 1 < alpha
         """
         if alpha != None:
-            self.alpha = alpha
+            self.alpha = float(alpha)
         elif center_of_mass != None:
-            self.alpha = 1./(1.+center_of_mass)
+            self.alpha = 1. / (1. + center_of_mass)
         else:
-            raise ValueError( "alpha or center_of_mass must be provided" )
-        if self.alpha <= 0 or 1 <= self.alpha:
-            raise ValueError( "alpha should satisfy 0<alpha<1: but alpha={}".format( self.alpha ) )
-        self.exponential_moving_average = initial_value
+            raise ValueError("`alpha` or `center_of_mass` must be provided")
+        if self.alpha < 0 or 1 < self.alpha:
+            raise ValueError("`alpha` must satisfy 0 <= `alpha` <= 1: but `alpha`={}".format(self.alpha))
+        self.value = float(value)
 
-    def add_value( self, value ):
-        """Add a sample to this ExponentialMovingAverage, and get the next average.
+    def add_value(self, new_value):
+        """Add a sample to this :obj:`ExponentialMovingAverage`, and update the average.
 
         Args:
-            value: number; the next value to contribute to the exponential moving average.
+            new_value (:obj:`float`): the next value to contribute to the exponential moving average
 
         Returns:
-            The next exponential moving average.
+            :obj:`float`: the updated exponential moving average
         """
-        self.exponential_moving_average = (self.alpha * value) \
-            + (1-self.alpha)*self.exponential_moving_average
-        return self.exponential_moving_average
+        self.value = (self.alpha * new_value) + (1. - self.alpha) * self.value
+        return self.value
 
-    def get_value( self ):
-        """Get the curent next average.
+    def get_value(self):
+        """ Get the curent average
 
         Returns:
-            The curent exponential moving average.
+            :obj:`float`: curent exponential moving average
         """
-        return self.exponential_moving_average
+        return self.value
+
+
+def weighted_percentile(values, weights, percentile):
+    """ Calculate percentile of a list of values, weighted by :obj:`weights`
+
+    Args:
+        values (:obj:`list` of :obj:`float`): values
+        weights (:obj:`list` of :obj:`float`): weights
+        percentile (:obj:`float`): percentile
+
+    Returns:
+        :obj:`float`: weighted percentile of :obj:`values`
+    """
+    values = 1. * numpy.array(values)
+    weights = 1. * numpy.array(weights)
+
+    values = numpy.extract(weights > 0, values)
+    weights = numpy.extract(weights > 0, weights)
+    if values.size == 0:
+        return numpy.nan
+
+    ind = numpy.argsort(values)
+    sorted_values = values[ind]
+    sorted_weights = weights[ind]
+    total_weight = sorted_weights.sum()
+
+    probabilities = sorted_weights.cumsum() / total_weight
+
+    ind = numpy.searchsorted(probabilities, percentile / 100.)
+    if probabilities[ind] == percentile / 100.:
+        return numpy.mean(sorted_values[ind:ind+2])
+    else:
+        return sorted_values[ind]
+
+
+def weighted_median(values, weights):
+    """ Calculate the median of a list of values, weighted by :obj:`weights`
+
+    Args:
+        values (:obj:`list` of :obj:`float`): values
+        weights (:obj:`list` of :obj:`float`): weights
+
+    Returns:
+        :obj:`float`: weighted median of :obj:`values`
+    """
+    values = 1. * numpy.array(values)
+    weights = 1. * numpy.array(weights)
+
+    values = numpy.extract(weights > 0, values)
+    weights = numpy.extract(weights > 0, weights)
+    if values.size == 0:
+        return numpy.nan
+
+    ind = numpy.argsort(values)
+    sorted_values = values[ind]
+    sorted_weights = weights[ind]
+    total_weight = sorted_weights.sum()
+
+    probabilities = sorted_weights.cumsum() / total_weight
+
+    ind = numpy.searchsorted(probabilities, 0.5)
+    if probabilities[ind] == 0.5:
+        return numpy.mean(sorted_values[ind:ind+2])
+    else:
+        return sorted_values[ind]

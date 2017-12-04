7689801071af625b1ad0b194cccc8abe0eee3aa3
Jonathan Karr <jonrkarr@gmail.com>
2017-12-04 16:50:22 -0500

    debugging pygit2 setup

--- setup.py
+++ setup.py
@@ -20,29 +20,33 @@ dirname = os.path.dirname(__file__)
 md = pkg_utils.get_package_metadata(dirname, name)
 
 # set needed pygit2 version
-import re
-libgit2_path = os.getenv("LIBGIT2")
-if not libgit2_path:
-    if os.name == 'nt':
-        libgit2_path = os.path.join(os.getenv("ProgramFiles"), 'libgit2')
+try:
+    import pygit2
+except:
+    import re
+    import warnings
+    libgit2_path = os.getenv("LIBGIT2")
+    if not libgit2_path:
+        if os.name == 'nt':
+            libgit2_path = os.path.join(os.getenv("ProgramFiles"), 'libgit2')
+        else:
+            libgit2_path = '/usr/local'
+    version_filename = os.path.join(libgit2_path, 'include', 'git2', 'version.h')
+
+    libgit2_version = None
+    if os.path.isfile(version_filename):
+        with open(version_filename, 'r') as file:
+            for line in file:
+                match = re.findall('define *LIBGIT2_VERSION *"(.*?)"', line.strip())
+                if match:
+                    libgit2_version = match[0]
+                    break
+
+    if libgit2_version:
+        md.extras_require['git'] = 'pygit2 == {}'.format(libgit2_version)
     else:
-        libgit2_path = '/usr/local'
-version_filename = os.path.join(libgit2_path, 'include', 'git2', 'version.h')
-
-libgit2_version = None
-if os.path.isfile(version_filename):
-    with open(version_filename, 'r') as file:
-        for line in file:
-            match = re.findall('define *LIBGIT2_VERSION *"(.*?)"', line.strip())
-            if match:
-                libgit2_version = match[0]
-                break
-
-if libgit2_version:
-    md.extras_require['git'] = 'pygit2 == {}'.format(libgit2_version)
-else:
-    warnings.warn(('wc_utils requires libgit2. Please install libgit2 and then retry installing '
-                   'wc_utils. Please see https://libgit2.github.com for installation instructions.'))
+        warnings.warn(('wc_utils requires libgit2. Please install libgit2 and then retry installing '
+                       'wc_utils. Please see https://libgit2.github.com for installation instructions.'))
 
 # install package
 setuptools.setup(

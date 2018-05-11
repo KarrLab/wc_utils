f83163afb8ecb03210fdd7692afef08d64f5b665
Jonathan Karr <jonrkarr@gmail.com>
2018-05-11 12:32:12 -0400

    adding util to copy directories when destination already existing

--- wc_utils/util/__init__.py
+++ wc_utils/util/__init__.py
@@ -3,6 +3,7 @@ from . import decorate_default_data_struct
 from . import dict
 from . import enumerate
 from . import environ
+from . import files
 from . import git
 from . import list
 from . import misc

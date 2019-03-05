f959128a12df23201ab29d4dbffe5ad753cc1052
Jonathan Karr <jonrkarr@gmail.com>
2019-03-05 02:37:50 -0500

    adding drawing

--- wc_utils/util/chem/compile_marvin.sh
+++ wc_utils/util/chem/compile_marvin.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 pushd wc_utils/util/chem/
 javac GetMajorMicroSpecies.java
+javac DrawMolecule.java
 jar -cvf GetMajorMicroSpecies.jar GetMajorMicroSpecies.class
+jar -cvf DrawMolecule.jar DrawMolecule.class
 popd

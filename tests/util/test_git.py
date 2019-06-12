a2808b694c5aa4087cf4d242544d06792e738df4
Arthur Goldberg <artgoldberg@gmail.com>
2019-06-12 09:30:46 -0400

    Foundation to ensure that that Git versions recorded in obj_model files refer to committed changes that are likely to be stored on the origin server; use either search_parent_directories or iterate over parent_dirnames, but not both because then its O(n**2)

--- tests/util/test_git.py
+++ tests/util/test_git.py
@@ -1,30 +1,96 @@
 """ Test of Git utilities
 
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
 :Author: Jonathan Karr <jonrkarr@gmail.com>
 :Date: 2017-05-24
-:Copyright: 2017-2018, Karr Lab
+:Copyright: 2017-2019, Karr Lab
 :License: MIT
 """
 
-from wc_utils.util import git
+import importlib
+gitpython = importlib.import_module('git', package='gitpython')
+from wc_utils.util.git import get_repo, get_repo_metadata, repo_status, RepoMetadataCollectionType
 import shutil
 import tempfile
+import os
 import unittest
+from pathlib import Path
 
 
 class TestGit(unittest.TestCase):
 
-    def test(self):
-        md = git.get_repo_metadata(dirname='.')
+    def test_get_repo(self):
+        repo = get_repo(dirname='.')
+        self.assertTrue(isinstance(repo, gitpython.Repo))
+        repo = get_repo(dirname=os.path.dirname(__file__), search_parent_directories=False)
+        self.assertTrue(isinstance(repo, gitpython.Repo))
+        repo = get_repo(dirname=os.path.dirname(__file__), search_parent_directories=True)
+        self.assertTrue(isinstance(repo, gitpython.Repo))
+
+        tempdir = tempfile.mkdtemp()
+        with self.assertRaisesRegex(ValueError, 'is not in a Git repository'):
+            get_repo(dirname=tempdir)
+        shutil.rmtree(tempdir)
+
+    def test_repo_status(self):
+        tempdir = tempfile.mkdtemp()
+        repo = gitpython.Repo.init(tempdir)
+        # create & commit a test file
+        Path(tempdir).joinpath('test_dir').mkdir()
+        test_file = str(Path(tempdir) / 'test_dir' / 'test.txt')
+        open(test_file, 'wb').close()
+        repo.index.add([test_file])
+        repo.index.commit("commit test_file")
+
+        # both data_repo and schema_repo should return True on a clean repo
+        self.assertTrue(repo_status(repo, RepoMetadataCollectionType.DATA_REPO,
+            data_file=test_file))
+        self.assertTrue(repo_status(repo, RepoMetadataCollectionType.SCHEMA_REPO))
+
+        # modify the file
+        with open(test_file, 'a') as f:
+            f.write('hello world!')
+        # schema_repo should return False
+        self.assertFalse(repo_status(repo, RepoMetadataCollectionType.SCHEMA_REPO))
+        # data_repo with file as data_file should return True
+        self.assertTrue(repo_status(repo, RepoMetadataCollectionType.DATA_REPO,
+            data_file=test_file))
+        # data_repo with other file as data_file should return False
+        other_test_file = str(Path(repo.git_dir).parent / 'other_test.txt')
+        self.assertFalse(repo_status(repo, RepoMetadataCollectionType.DATA_REPO,
+            data_file=other_test_file))
+        # commit changes
+        repo.index.add([test_file])
+        repo.index.commit("commit changes to test_file")
+
+        # create an untracked file
+        untracked_file = str(Path(tempdir) / 'test_dir' / 'untracked_file.txt')
+        open(untracked_file, 'wb').close()
+        # schema_repo should return False
+        self.assertFalse(repo_status(repo, RepoMetadataCollectionType.SCHEMA_REPO))
+        # data_repo with data_file=untracked_file should return True
+        self.assertTrue(repo_status(repo, RepoMetadataCollectionType.DATA_REPO,
+            data_file=untracked_file))
+        # data_repo with data_file=other_test_file should return False
+        self.assertFalse(repo_status(repo, RepoMetadataCollectionType.DATA_REPO,
+            data_file=other_test_file))
+
+        # data_repo with file not in repo should raise exception
+        with self.assertRaisesRegex(ValueError, r"data_file '.+' must be in the repo that's in '.+'"):
+            repo_status(repo, RepoMetadataCollectionType.DATA_REPO, data_file='/tmp/test.xlsx')
+
+        shutil.rmtree(tempdir)
+
+        repo = get_repo(dirname='.')
+        with self.assertRaisesRegex(ValueError, "data_file must be provided if repo_type is "
+                "RepoMetadataCollectionType.DATA_REPO"):
+            repo_status(repo, RepoMetadataCollectionType.DATA_REPO)
+
+    def test_get_repo_metadata(self):
+        md = get_repo_metadata(dirname='.')
         self.assertIn(md.url, [
             'https://github.com/KarrLab/wc_utils.git',
             'ssh://git@github.com/KarrLab/wc_utils.git',
             'git@github.com:KarrLab/wc_utils.git',
         ])
         self.assertEqual(md.branch, 'master')
-
-    def test_no_repo(self):
-        tempdir = tempfile.mkdtemp()
-        with self.assertRaisesRegex(ValueError, 'is not a Git repository'):
-            git.get_repo_metadata(dirname=tempdir)
-        shutil.rmtree(tempdir)
--- wc_utils/util/git.py
+++ wc_utils/util/git.py
@@ -1,8 +1,9 @@
-""" Git utilities
+""" Git utilities for obtaining repo metadata
 
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
 :Author: Jonathan Karr <jonrkarr@gmail.com>
 :Date: 2017-05-24
-:Copyright: 2017-2018, Karr Lab
+:Copyright: 2017-2019, Karr Lab
 :License: MIT
 """
 
@@ -10,27 +11,30 @@ import importlib
 git = importlib.import_module('git', package='gitpython')
 import itertools
 import os
-import pathlib
+from pathlib import Path
+from enum import Enum, auto
 
 
-def get_repo_metadata(dirname='.', search_parent_directories=True):
-    """ Get meta data about a repository
+def get_repo(dirname='.', search_parent_directories=True):
+    """ Get a Git repository
 
     Args:
         dirname (:obj:`str`): path to Git repository
         search_parent_directories (:obj:`bool`, optional): if :obj:`True`, search for the root 
-            of the repository among the parent directories of :obj:`dirname`
+            of the repository among the parent directories of :obj:`dirname`; default=:obj:`True`
 
     Returns:
-        :obj:`RepositoryMetadata`: repository meta data
+        :obj:`git.Repo`: a `GitPython` repository
 
     Raises:
         :obj:`ValueError`: if obj:`dirname` is not a path to a Git repository
     """
     repo = None
-    pure_path = pathlib.PurePath(os.path.abspath(dirname))
-    parent_dirnames = itertools.chain([os.path.abspath(dirname)], pure_path.parents)
-    for parent_dirname in parent_dirnames:
+    resolved_path = Path(dirname).expanduser().resolve()
+    dirnames = itertools.chain([str(resolved_path)], resolved_path.parents)
+    if search_parent_directories:
+        dirnames = [str(resolved_path)]
+    for parent_dirname in dirnames:
         try:
             repo = git.Repo(str(parent_dirname), search_parent_directories=search_parent_directories)
             break
@@ -38,7 +42,91 @@ def get_repo_metadata(dirname='.', search_parent_directories=True):
             pass
 
     if not repo:
-        raise ValueError('"{}" is not a Git repository'.format(dirname))
+        raise ValueError('"{}" is not in a Git repository'.format(dirname))
+    return repo
+
+
+class RepoMetadataCollectionType(Enum):
+    """ Type of Git repo being queried for metadata that's stored in a data file """
+    DATA_REPO = auto()
+    SCHEMA_REPO = auto()
+
+
+def repo_status(repo, repo_type, data_file=None):
+    """ Get status of a repo
+
+    Args:
+        repo (:obj:`git.Repo`): a `GitPython` repository
+        repo_type (:obj:`RepoMetadataCollectionType`): repo type that's being tested
+        data_file (:obj:`str`, optional): pathname of a data file in the repo; must be provided if
+            `repo_type` is `RepoMetadataCollectionType.DATA_REPO`
+
+    Returns:
+        :obj:`bool`: whether the repo is in a state that's suitable for collecting metadata for the
+            `repo_type`
+    """
+    diff_index = repo.index.diff(None)
+    if repo_type is RepoMetadataCollectionType.DATA_REPO:
+
+        if not data_file:
+            raise ValueError("data_file must be provided if repo_type is "
+                "RepoMetadataCollectionType.DATA_REPO")
+
+        # ensure that data_file exists in repo
+        resolved_data_file = Path(data_file).expanduser().resolve()
+        repo_root = Path(repo.git_dir).parent
+        try:
+            resolved_data_file.relative_to(str(repo_root))
+        except ValueError:
+            raise ValueError("data_file '{}' must be in the repo that's in '{}'".format(data_file, str(repo_root)))
+
+        # a data repo that's suitable for collecting metadata may only have differences between the
+        # index and the working tree in the data_file
+        for change_type in diff_index.change_type:
+            for diff in diff_index.iter_change_type(change_type):
+                resolved_a_rawpath = repo_root.joinpath(diff.a_rawpath.decode())
+                resolved_b_rawpath = repo_root.joinpath(diff.b_rawpath.decode())
+                if (resolved_a_rawpath != resolved_data_file or
+                    resolved_b_rawpath != resolved_data_file):
+                    return False
+
+        for untracked_file in repo.untracked_files:
+            if repo_root.joinpath(untracked_file) != resolved_data_file:
+                return False
+
+        return True
+
+    elif repo_type is RepoMetadataCollectionType.SCHEMA_REPO:
+
+        # a schema repo that has any differences between the index and the working tree
+        # isn't suitable for collecting metadata
+        for change_type in diff_index.change_type:
+            if list(diff_index.iter_change_type(change_type)):
+                return False
+        if repo.untracked_files:
+            return False
+        return True
+
+    else:   # pragma: no cover
+        raise ValueError("Invalid RepoMetadataCollectionType: '{}'".format(repo_type.name))
+
+
+def get_repo_metadata(dirname='.', search_parent_directories=True):
+    """ Get metadata about a Git repository
+
+    Args:
+        dirname (:obj:`str`): path to Git repository
+        search_parent_directories (:obj:`bool`, optional): if :obj:`True`, have `GitPython` search for
+            the root of the repository among the parent directories of :obj:`dirname`
+
+    Returns:
+        :obj:`RepositoryMetadata`: repository meta data
+
+    Raises:
+        :obj:`ValueError`: if obj:`dirname` is not a path to a Git repository
+    """
+    repo = get_repo(dirname=dirname, search_parent_directories=search_parent_directories)
+
     url = str(repo.remote('origin').url)
     branch = str(repo.active_branch.name)
     revision = str(repo.head.commit.hexsha)
